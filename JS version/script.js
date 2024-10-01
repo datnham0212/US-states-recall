6b32379c09c92062381182ccf927ac670a3d30fe
datnham0212 <90122319+datnham0212@users.noreply.github.com>
2024-10-01 23:58:27 +0700

    Add files via upload

--- JS version/script.js
+++ JS version/script.js
@@ -1,52 +1,7 @@
-// const us_states = [
-//     "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware",
-//     "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky",
-//     "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi",
-//     "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico",
-//     "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania",
-//     "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont",
-//     "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
-// ];
-
-//     let us_states_lowercase = us_states.map(function(item){
-//         return item.toLowerCase();
-//     });
-
-//     let visited = [];
-
-//     var us_states_length = us_states.length;
-
-//     var input = document.getElementById('states');
-//     var count = 0;
-//     input.addEventListener("keypress", function(event){
-//         if (event.key == "Enter"){
-//             var state = input.value;
-//             let temp = state.toLowerCase();
-//             if(us_states_lowercase.includes(temp) && !visited.includes(temp)){
-//                 visited.push(temp);
-//                 let temp_index = us_states_lowercase.indexOf(temp);
-//                 let node = document.createElement('p');
-//                 let textnode = document.createTextNode(us_states[temp_index]);
-//                 node.append(textnode);
-//                 document.getElementById('result').appendChild(node);
-//                 us_states.splice(us_states.indexOf(state), 0);
-//                 count++;
-//                 if(count == us_states_length){
-//                 alert('Congrats, you have listed all of the states!');
-//                 }
-//             }
-//             else{
-//                 alert('Try again!');
-//             }
-//             document.getElementById('states').value = '';
-//         }
-//     });
-
-
 const canvas = document.getElementById('us_map');
 const context = canvas.getContext('2d');
 const image = new Image();
-image.src = 'https://www.50states.com/wp-content/uploads/2020/12/US-Blank-map.jpg';
+image.src = 'US-Blank-map.jpg';
 
 image.onload = () => {
     context.drawImage(image, 0, 0)
@@ -62,7 +17,7 @@ canvas.addEventListener('click', (event) => {
 
 
 const statesAb = {
-    'California': 'CA',
+    'California': ['CA', 80, 257],
     'New York': 'NY',
     'Texas': 'TX',
     'Florida': 'FL',
@@ -74,7 +29,7 @@ const statesAb = {
     'Michigan': 'MI',
     'New Jersey': 'NJ',
     'Virginia': 'VA',
-    'Washington': 'WA',
+    'Washington': ['WA', 125, 80],
     'Arizona': 'AZ',
     'Massachusetts': 'MA',
     'Tennessee': 'TN',
@@ -88,7 +43,7 @@ const statesAb = {
     'Alabama': 'AL',
     'Louisiana': 'LA',
     'Kentucky': 'KY',
-    'Oregon': 'OR',
+    'Oregon': ['OR', 100, 135],
     'Oklahoma': 'OK',
     'Connecticut': 'CT',
     'Utah': 'UT',
@@ -101,28 +56,28 @@ const statesAb = {
     'Nebraska': 'NE',
     'West Virginia': 'WV',
     'Idaho': 'ID',
-    'Hawaii': 'HI',
+    'Hawaii': ['HI', 255, 464],
     'New Hampshire': 'NH',
     'Maine': 'ME',
-    'Montana': 'MT',
+    'Montana': ['MT', 245, 110],
     'Rhode Island': 'RI',
     'Delaware': 'DE',
     'South Dakota': 'SD',
-    'Alaska': 'AK',
+    'Alaska': ['AK', 105, 430],
     'Vermont': 'VT',
     'Wyoming': 'WY',
     'North Dakota': 'ND'    
 };
 
-// alert(Object.keys(statesAb).length)
 
 document.getElementById('recall').addEventListener('click', () => {
     const states = document.getElementById('statesInput').value
-    const abb = statesAb[states]
+    const abb = statesAb[states][0]
+    const abb_x = statesAb[states][1]
+    const abb_y = statesAb[states][2]
 
     if (abb) {
-        // context.fillStyle = 'red';
         context.font = '30px Arial';
-        context.fillText(abb, 128, 80);
+        context.fillText(abb, abb_x, abb_y);
     }
 })
